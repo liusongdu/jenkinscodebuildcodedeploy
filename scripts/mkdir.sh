40b36d8099aad63b21e7e7558f6d34899c5efdba
Leo <liusongdu@hotmail.com>
2021-06-14 05:28:04 +0000

    t

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,4 +1,6 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3.7 get-pip.py --user
-python3.7 -m pip install Flask
+python3 get-pip.py --user
+python3 -m pip install Flask requests
+sudo chmod +x /web/scripts/start_flask.sh
+sudo chmod +x /web/scripts/stop_flask1.sh
