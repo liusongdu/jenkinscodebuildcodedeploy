a2652a823817083b2ab7499f95600f6ffa36a774
EC2 Default User <ec2-user@ip-10-0-0-156.us-west-2.compute.internal>
2021-06-14 11:10:19 +0000

    another test

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -2,5 +2,3 @@ mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 python3 -m pip install Flask requests
-sudo chmod +x /web/scripts/start_flask.sh
-sudo chmod +x /web/scripts/stop_flask1.sh
